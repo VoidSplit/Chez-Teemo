71884389f156017d294c4d8eeff0d2fc77e480b5
VoidSplit <46133265+VoidSplit@users.noreply.github.com>
2023-04-30 16:13:18 +0200

    first commit

---
+++ res/js/app.js
@@ -0,0 +1,119 @@
+console.log("script loaded")
+async function loadJSON(path, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success) success(JSON.parse(xhr.responseText));
+            } else {
+                if (error) error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", path, true);
+    xhr.send();
+}
+const getCarte = () => {
+    return new Promise((resolve, reject) => loadJSON('data/carte.json', function (data) {
+        let aperitifs = data.aperitifs
+        let plats = data.plats
+        let boissons = data.boissons
+        let desserts = data.desserts
+        resolve([aperitifs, plats, boissons, desserts]);
+    }, function (xhr) {
+        reject(xhr);
+    })
+    );
+}
+const createCarteDOM = (carte) => {
+    console.log(carte)
+    carte.forEach((m, i) => {
+        console.log(m, i)
+
+
+        let categoryDisplay = ""
+        let categoryList = []
+
+        switch(i) {
+            case 0:
+                categoryDisplay = "Apéritifs"
+                m.champignons.forEach(item => {
+                    let newItem = {
+                        name: item,
+                        description: ""
+                    }
+                    categoryList.push(newItem)
+                })
+                m.sansChampignons.forEach(item => {
+                    let newItem = {
+                        name: item
+                    }
+                    categoryList.push(newItem)
+                })
+                break;
+            case 1:
+                categoryDisplay = "Plats"
+                m.champignons.forEach(item => {
+                    let newItem = {
+                        name: item
+                    }
+                    categoryList.push(newItem)
+                })
+                m.sansChampignons.forEach(item => {
+                    let newItem = {
+                        name: item
+                    }
+                    categoryList.push(newItem)
+                })
+                break;
+            case 2:
+                categoryDisplay = "Boissons"
+                m.alcool.forEach(item => {
+                    let newItem = {
+                        name: item
+                    }
+                    categoryList.push(newItem)
+                })
+                
+                break;
+            case 3:
+                categoryDisplay = "Desserts"
+                m.forEach(item => {
+                    let newItem = {
+                        name: item
+                    }
+                    categoryList.push(newItem)
+                })
+                break;
+        }
+        
+        let carteLayout = document.getElementById("carte-layout")
+
+        let category = document.createElement('div')
+        let categoryName = document.createElement('div')
+        category.classList = "category"
+        
+        categoryName.classList = "name"
+        categoryName.innerText = categoryDisplay
+        category.append(categoryName)
+
+        categoryList.forEach(el => {
+            let article = document.createElement("article")
+            article.innerHTML = `
+                <p>${el.name}</p>
+            `
+            category.append(article)
+        });
+        carteLayout.append(category)
+    })
+}
+const init = async () => {
+    const data = getCarte();
+    let infos;
+    await data.then(a => infos = a)
+    if(infos) {
+        createCarteDOM(infos)
+    }
+}
+
+init()
